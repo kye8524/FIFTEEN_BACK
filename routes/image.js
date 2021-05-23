10030628c8fcd0c374066b21affd8292113831e4
kye8524 <kye8314@gmail.com>
2021-05-24 03:12:35 +0900

    fix

--- routes/image.js
+++ routes/image.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const upload = require('fileupload');
+const upload = require('./fileupload');
 const multer = require('multer');
 
 const router = express.Router();
